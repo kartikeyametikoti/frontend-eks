aa1408257f7e44beafc9d11a0d6041e9713ecda6
kartikeyametikoti <149247879+kartikeyametikoti@users.noreply.github.com>
2025-03-24 11:47:08 +0530

    Update Signup.js

--- src/Signup.js
+++ src/Signup.js
@@ -14,7 +14,7 @@ function Signup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://a5b932b222704417ba8c156b8398fdbb-1366802763.us-east-1.elb.amazonaws.com/api/signup", formData);
+      const response = await axios.post("http://a47214a85e8ae4ef6b15c1e8785bd139-2096017316.us-east-1.elb.amazonaws.com/api/signup", formData);
       alert(response.data.message);
       navigate("/login");
     } catch (error) {
